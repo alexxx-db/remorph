647d0c1bd3281924c796b2b1a135596b611d3c81
Jim Idle <jim.idle@databricks.com>
2024-06-12 08:15:16 -0600

    TSQL: Update the SELECT statement to support XML workspaces (#451)
    Here we upgrade the TSQL Select statement grammar to support
    XMLWORKSPACES correctly according to the latest specification.
    
    Databricks SQL does not support XMLWORKSPACES, so for now this is a
    grammar only change so that we can at least cover the syntax.
    
    ---------
    
    Co-authored-by: Valentin Kasas <valentin.kasas@xebia.com>

--- tests/resources/functional/tsql/select/test_cte_xml.sql
+++ tests/resources/functional/tsql/select/test_cte_xml.sql
@@ -12,4 +12,14 @@ WHERE col2 = 'xyz'
 FOR XML RAW ('namespace:namespace'), ELEMENTS;
 
 -- databricks sql:
+<<<<<<< HEAD
 WITH XMLNAMESPACES ('somereference' as namespace) SELECT col1 as 'namespace:col1', col2 as 'namespace:col2' FROM  Table1 WHERE col2 = 'xyz' FOR XML RAW ('namespace:namespace'), ELEMENTS;
+=======
+
+WITH XMLNAMESPACES ('somereference' as namespace)
+SELECT col1 as 'namespace:col1',
+       col2      as 'namespace:col2'
+FROM  Table1
+WHERE col2 = 'xyz'
+FOR XML RAW ('namespace:namespace'), ELEMENTS;
+>>>>>>> b6255e0b (TSQL: Update the SELECT statement to support XML workspaces (#451))
