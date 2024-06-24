06b282bf0a53e703c42acdbbf0ed19e626d10c5b
Valentin Kasas <valentin.kasas@xebia.com>
2024-06-24 18:53:44 +0200

    Improve Snowflake coverage test success rate  (#485)
    By removing unused tokens, adding more nonReservedKeywords and defining
    some functions

---
+++ tests/resources/functional/snowflake/test_array_position/test_array_position_1.sql
@@ -0,0 +1,6 @@
+
+-- snowflake sql:
+SELECT array_position(col1, array1) AS array_position_col1 FROM tabl;
+
+-- databricks sql:
+SELECT ARRAY_POSITION(col1, array1) AS array_position_col1 FROM tabl;
---
+++ tests/resources/functional/snowflake/test_datediff/test_datediff_11.sql
@@ -0,0 +1,6 @@
+
+-- snowflake sql:
+SELECT DATEDIFF('DAY', start, end);
+
+-- databricks sql:
+SELECT DATEDIFF(day, start, end);
