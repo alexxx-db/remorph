7bf88b0e485dece0f1cf56d87952ce8807779da5
Jim Idle <jim.idle@databricks.com>
2024-08-02 11:53:23 -0600

    Implement TSQL specific function call mapper (#765)
    Here, we implement the function call mapper (Rule) that allows us to
    make complex transformations from TSQL specific functions into
    Databricks compatible constructs. Transformations are sometimes argument
    reordering, and sometimes replacement with a different expression.
    
    In this PR we use the transformation of TSQL `DATEADD` to Databricks SQL
    `DATE_ADD`, `ADD_MONTHS` and `xxx + INTERVAL n {days|months|etc}`
    
    For example the TSQL:
    
    ```tsql
    SELECT DATEADD(hh, 7, col1) AS add_hours_col1 FROM tabl;
    ```
    
    Translates to:
    
    ```sql
    SELECT col1 + INTERVAL 7 HOUR AS add_hours_col1 FROM tabl;
    ```

--- tests/resources/functional/tsql/functions/test_dateadd_microsecond_1.sql
+++ tests/resources/functional/tsql/functions/test_dateadd_microsecond_1.sql
@@ -8,7 +8,11 @@ SELECT DATEADD(MICROSECOND, 7, col1) AS add_microsecond_col1 FROM tabl;
 
 -- databricks sql:
 <<<<<<< HEAD
+<<<<<<< HEAD
 SELECT col1 + INTERVAL 7 MICROSECOND AS add_microsecond_col1 FROM tabl;
 =======
 SELECT (col1 + INTERVAL 7 MICROSECOND) AS add_microsecond_col1 FROM tabl;
 >>>>>>> 5126eaff (Improve test coverage for TSQL remorph (#439))
+=======
+SELECT col1 + INTERVAL 7 MICROSECOND AS add_microsecond_col1 FROM tabl;
+>>>>>>> 24a1e298 (Implement TSQL specific function call mapper (#765))
