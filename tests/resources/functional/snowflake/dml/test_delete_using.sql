b6aeafb80eb4900adcd26e31955617c2d64bf99c
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-08-20 19:13:22 +0200

    [sql] generate `DELETE FROM ...` (#824)

--- tests/resources/functional/snowflake/dml/test_delete_using.sql
+++ tests/resources/functional/snowflake/dml/test_delete_using.sql
@@ -3,4 +3,8 @@
 DELETE FROM t1 USING t2 WHERE t1.c1 = t2.c2;
 
 -- databricks sql:
+<<<<<<< HEAD
 MERGE INTO  t1 USING t2 ON t1.c1 = t2.c2 WHEN MATCHED THEN DELETE;
+=======
+MERGE INTO  t1 USING t2 ON t1.c1 = t2.c2 WHEN MATCHED THEN DELETE;
+>>>>>>> ce7e4835 ([sql] generate `DELETE FROM ...` (#824))
