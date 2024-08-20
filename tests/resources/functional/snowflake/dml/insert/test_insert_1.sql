a7ae10443c2b07112b321b0e4f22206c51eaca8f
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-08-20 18:24:05 +0200

    [sql] generate `INSERT INTO ...` (#823)

--- tests/resources/functional/snowflake/dml/insert/test_insert_1.sql
+++ tests/resources/functional/snowflake/dml/insert/test_insert_1.sql
@@ -2,4 +2,8 @@
 INSERT INTO foo VALUES (1, 'bar', true), (1, 'qux', false);
 
 -- databricks sql:
+<<<<<<< HEAD
 INSERT INTO foo VALUES (1, 'bar', true), (1, 'qux', false);
+=======
+INSERT INTO foo VALUES (1, 'bar', true), (1, 'qux', false);
+>>>>>>> b05807fa ([sql] generate `INSERT INTO ...` (#823))
