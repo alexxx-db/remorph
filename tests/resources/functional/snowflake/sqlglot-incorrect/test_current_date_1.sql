05fdda6cc7652968ed79f3b563d2634b0499e7c2
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-08-21 16:27:17 +0200

    [snowflake] more cleanup of functional tests (#834)

--- tests/resources/functional/snowflake/sqlglot-incorrect/test_current_date_1.sql
+++ tests/resources/functional/snowflake/sqlglot-incorrect/test_current_date_1.sql
@@ -2,4 +2,8 @@
 SELECT CURRENT_DATE() FROM tabl;
 
 -- databricks sql:
+<<<<<<< HEAD
 SELECT CURRENT_DATE() FROM tabl;
+=======
+SELECT CURRENT_DATE FROM tabl;
+>>>>>>> 18b6f8d9 ([snowflake] more cleanup of functional tests (#834))
