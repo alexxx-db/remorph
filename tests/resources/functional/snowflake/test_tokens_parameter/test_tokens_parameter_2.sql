ca9902672d86a230e40798174b3bc17c89e2fcc7
Jim Idle <jim.idle@databricks.com>
2024-08-30 08:00:30 -0600

    Minor upgrades to Snowflake parameter processing (#871)
    NB: Previous PR was merged before I was totally finished
    
    Normalizes parameter generation to always use ${} for clarity and
    conforming to Databricks notebook examples for widgets.
    
    Adds additional coverage test for variable refs in strings

--- tests/resources/functional/snowflake/test_tokens_parameter/test_tokens_parameter_2.sql
+++ tests/resources/functional/snowflake/test_tokens_parameter/test_tokens_parameter_2.sql
@@ -3,7 +3,11 @@ select emp_id from abc.emp where emp_id = $ids;
 
 -- databricks sql:
 <<<<<<< HEAD
+<<<<<<< HEAD
 select emp_id from abc.emp where emp_id = $ids;
 =======
 select emp_id from abc.emp where emp_id = $ids;
 >>>>>>> 96c6764d (Added Translation Support for `!` as `commands` and `&` for `Parameters` (#771))
+=======
+select emp_id from abc.emp where emp_id = $ids;
+>>>>>>> f18b4594 (Minor upgrades to Snowflake parameter processing (#871))
