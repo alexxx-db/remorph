d5ae649eaec8dd1681c07d4a782f636c6a897623
Jim Idle <jim.idle@databricks.com>
2024-10-29 15:52:33 -0600

    TSQL: prevent selectListElem from ignoring valid columns when errorNodes are accumulated (#1023)
    In code such as:
    
    ```tsql
    SELECT col1, , col2 ...
    ```
    
    Where the parser recovers nicely from the extraneous ',', the visit
    method would ignore the recovery and only return the errorNode output.
    
    Here we use a builder method rather than a visit method and return a
    Seq[] rather than just a single IR node. The generator then receives all
    the valid column definitions.
    
    We also modify the code generator such that it does not add ',' after
    the generated comments representing error nodes.

--- tests/resources/functional/tsql/core_engine/test_invalid_syntax/syntax_error_1.sql
+++ tests/resources/functional/tsql/core_engine/test_invalid_syntax/syntax_error_1.sql
@@ -7,10 +7,14 @@ select col1,, col2 from table_name;
 -- databricks sql:
 SELECT
 <<<<<<< HEAD
+<<<<<<< HEAD
   col1,
 =======
     col1,
 >>>>>>> 34e4a547 (Implement text gatherers for additional error types (#1020))
+=======
+  col1,
+>>>>>>> 4d9cf633 (TSQL: prevent selectListElem from ignoring valid columns when errorNodes are accumulated (#1023))
 /* The following issues were detected:
 
    Unparsed input - ErrorNode encountered
@@ -24,6 +28,10 @@ SELECT
     expecting one of: $Currency, 'String', @@Reference, @Local, Float, Identifier, Integer, Operator, Real, '$ACTION', '$NODE_ID', '$PARTITION'...
     Unparsable text: ,
  */
+<<<<<<< HEAD
 >>>>>>> 34e4a547 (Implement text gatherers for additional error types (#1020))
+=======
+  col2
+>>>>>>> 4d9cf633 (TSQL: prevent selectListElem from ignoring valid columns when errorNodes are accumulated (#1023))
 FROM
     table_name;
