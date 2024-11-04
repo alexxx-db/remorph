b336b7563b2070dc0b6a35b10e4eba6489eac1ce
Sriram Mohanty <69749553+sriram251-code@users.noreply.github.com>
2024-11-04 22:56:02 +0530

    TO_NUMBER/TO_DECIMAL/TO_NUMERIC without precision and scale (#1053)
    closes #1011
    
    This pull request improves the transpilation to handle cases where
    precision and scale are not specified for
    TO_NUMBER/TO_DECIMAL/TO_NUMERIC snowflake functions.
    Functions now automatically apply default values when parameters are
    omitted
    (https://docs.snowflake.com/en/sql-reference/functions/to_decimal)
    Precision defaults to 38 (maximum allowed)
    Scale defaults to 0.

--- tests/resources/functional/snowflake/functions/conversion/test_to_number/test_to_number_9.sql
+++ tests/resources/functional/snowflake/functions/conversion/test_to_number/test_to_number_9.sql
@@ -3,4 +3,8 @@
 select TO_NUMBER(EXPR) from test_tbl;
 
 -- databricks sql:
+<<<<<<< HEAD
 SELECT CAST(EXPR AS DECIMAL(38, 0)) FROM test_tbl;
+=======
+SELECT CAST(EXPR AS DECIMAL(38, 0)) FROM test_tbl;
+>>>>>>> cbea0c07 (TO_NUMBER/TO_DECIMAL/TO_NUMERIC without precision and scale (#1053))
