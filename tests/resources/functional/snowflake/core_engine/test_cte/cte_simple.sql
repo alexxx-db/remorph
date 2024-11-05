f3a4e6ddee38923fff8bf38ed9a8437108b0b46f
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2024-11-05 22:12:38 +0530

    With Recursive (#1000)
    Added grammar enhancements for With Recursive statements.

--- tests/resources/functional/snowflake/core_engine/test_cte/cte_simple.sql
+++ tests/resources/functional/snowflake/core_engine/test_cte/cte_simple.sql
@@ -13,4 +13,8 @@ SELECT *
 FROM employee_hierarchy;
 
 -- databricks sql:
+<<<<<<< HEAD
 WITH employee_hierarchy AS (SELECT employee_id, manager_id, employee_name FROM employees WHERE manager_id IS NULL) SELECT * FROM employee_hierarchy;
+=======
+WITH employee_hierarchy AS (SELECT employee_id, manager_id, employee_name FROM employees WHERE manager_id IS NULL) SELECT * FROM employee_hierarchy;
+>>>>>>> 205595c7 (With Recursive (#1000))
