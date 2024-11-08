cf567ab9d994716ad90bf748f11f66b265d2faed
Valentin Kasas <valentin.kasas@xebia.com>
2024-11-08 15:07:15 +0100

    Avoid processing escapes in interpolator arguments (#1167)
    Calling `treatEscapes` on the interpolator's arguments was a mistake
    (calling it on string parts wasn't).
    
    The reason is that string parts come from our scala program, like the
    `\n` in `code"$foo\n$bar"`, while arguments (may) come from the input
    query (and as such should be left as-is).
    
    ---------
    
    Co-authored-by: SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>

--- tests/resources/functional/snowflake/functions/strings/regexp_substr_2.sql
+++ tests/resources/functional/snowflake/functions/strings/regexp_substr_2.sql
@@ -6,5 +6,10 @@ select id, string1,
 -- databricks sql:
 
 select id, string1,
+<<<<<<< HEAD
 	   REGEXP_EXTRACT(string1, 'the\\W+\\w+', 0) as `SUBSTRING`
     from demo2;
+=======
+	   REGEXP_EXTRACT(string1, 'the\\W+\\w+') as `SUBSTRING`
+    from demo2;
+>>>>>>> 4486e58a (Avoid processing escapes in interpolator arguments (#1167))
