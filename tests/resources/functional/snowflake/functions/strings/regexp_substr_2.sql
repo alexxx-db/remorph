1e3307333dcbace87c107fd860174ccdd691567e
ganeshdogiparthi-db <84570155+ganeshdogiparthi-db@users.noreply.github.com>
2024-11-14 10:53:48 -0500

    Bump sqlglot from 25.8.1 to 25.35.0 (#1205)
    Co-authored-by: sundar.shankar <sundar.shankar@databricks.com>
    Co-authored-by: SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>

--- tests/resources/functional/snowflake/functions/strings/regexp_substr_2.sql
+++ tests/resources/functional/snowflake/functions/strings/regexp_substr_2.sql
@@ -7,9 +7,14 @@ select id, string1,
 
 select id, string1,
 <<<<<<< HEAD
+<<<<<<< HEAD
 	   REGEXP_EXTRACT(string1, 'the\\W+\\w+', 0) as `SUBSTRING`
     from demo2;
 =======
 	   REGEXP_EXTRACT(string1, 'the\\W+\\w+') as `SUBSTRING`
     from demo2;
 >>>>>>> 4486e58a (Avoid processing escapes in interpolator arguments (#1167))
+=======
+	   REGEXP_EXTRACT(string1, 'the\\W+\\w+', 0) as `SUBSTRING`
+    from demo2;
+>>>>>>> d4ad0cd2 (Bump sqlglot from 25.8.1 to 25.35.0 (#1205))
