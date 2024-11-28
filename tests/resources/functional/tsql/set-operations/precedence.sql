8b922589a88dae295b28c2e885c799792b8e8daa
Andrew Snare <asnare@users.noreply.github.com>
2024-11-28 15:35:49 +0100

    Implement remaining TSQL set operations. (#1227)
    This PR implements support for parsing set operations with TSql: `UNION
    [ALL]`, `EXCEPT` and `INTERSECT`.
    
    The grammar previously supported these but they were not being converted
    to the IR.
    
    Resolves #1126.
    Resolves #1102.
    
    - [x] added unit tests
    - [x] added transpiler tests
    - [x] added functional tests

--- tests/resources/functional/tsql/set-operations/precedence.sql
+++ tests/resources/functional/tsql/set-operations/precedence.sql
@@ -8,6 +8,7 @@
 -- tsql sql:
 
 -- Verifies UNION/EXCEPT as left-to-right, with brackets.
+<<<<<<< HEAD
 (SELECT 1
  UNION
  SELECT 2
@@ -49,20 +50,60 @@ UNION ALL
 
 -- databricks sql:
 
+=======
+SELECT 1
+UNION
+SELECT 2
+EXCEPT
+(SELECT 3
+ UNION ALL
+ SELECT 4)
+
+INTERSECT
+
+-- Verifies UNION/EXCEPT as left-to-right when the order is reversed.
+SELECT 5
+EXCEPT
+SELECT 6
+UNION
+SELECT 7
+
+INTERSECT
+
+-- Verifies brackets have precedence over INTERSECT.
+(SELECT 8
+ INTERSECT
+ SELECT 9)
+
+INTERSECT
+
+-- Not needed: just confirms helps us confirm that INTERSECT is also left-to-right.
+SELECT 10;
+
+
+-- databricks sql:
+>>>>>>> a5bbdb69 (Implement remaining TSQL set operations. (#1227))
     (
         (
             (
                 ((SELECT 1) UNION (SELECT 2))
             EXCEPT
+<<<<<<< HEAD
                 ((SELECT 3) UNION (SELECT 4))
             )
         UNION ALL
+=======
+                ((SELECT 3) UNION ALL (SELECT 4))
+            )
+        INTERSECT
+>>>>>>> a5bbdb69 (Implement remaining TSQL set operations. (#1227))
             (
                 ((SELECT 5) EXCEPT (SELECT 6))
             UNION
                 (SELECT 7)
             )
         )
+<<<<<<< HEAD
     UNION ALL
         (
             ((SELECT 8) UNION (SELECT 9))
@@ -76,3 +117,10 @@ UNION ALL
     INTERSECT
         ((SELECT 14) INTERSECT (SELECT 15))
     );
+=======
+    INTERSECT
+        ((SELECT 8) INTERSECT (SELECT 9))
+    )
+INTERSECT
+    (SELECT 10);
+>>>>>>> a5bbdb69 (Implement remaining TSQL set operations. (#1227))
