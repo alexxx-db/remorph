390f9c7ef20d19137a8e421a7b9bcb0080c6421a
radhikaathalye-db <135362260+radhikaathalye-db@users.noreply.github.com>
2025-10-31 11:33:32 -0700

    Add method to upload DuckDB files to Unity Catalog Volume with tests (#2124)
    <!-- REMOVE IRRELEVANT COMMENTS BEFORE CREATING A PULL REQUEST -->
    ## Changes
    <!-- Summary of your changes that are easy to understand. Add
    screenshots when necessary, they're helpful to illustrate the before and
    after state -->
    ### What does this PR do?
    Added functionality for uploading the duck db extract to a UC volume
    ### Relevant implementation details
    
    ### Caveats/things to watch out for when reviewing:
    
    ### Linked issues
    <!-- DOC: Link issue with a keyword: close, closes, closed, fix, fixes,
    fixed, resolve, resolves, resolved. See
    https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
    -->
    
    Resolves #..
    
    ### Functionality
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs lakebridge ...`
    - [ ] ... +add your own
    
    ### Tests
    <!-- How is this tested? Please see the checklist below and also
    describe any other relevant tests -->
    
    - [ ] manually tested
    - [x] added unit tests
    - [ ] added integration tests
    
    ---------
    
    Co-authored-by: Will Girten <will.girten@databricks.com>
    Co-authored-by: sundarshankar89 <sundar.shankar@databricks.com>

--- tests/integration/assessments/test_dashboard_manager.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from .utils.profiler_extract_utils import build_mock_synapse_extract
-
-
-@pytest.fixture(scope="module")
-def mock_synapse_profiler_extract():
-    synapse_extract_path = build_mock_synapse_extract("mock_profiler_extract")
-    return synapse_extract_path
